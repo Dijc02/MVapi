59e701e3911c708e4e67f7fa262ddb0972fbf950
Dijc02 <78810738+Dijc02@users.noreply.github.com>
2024-02-16 16:15:40 +0100

    MVapi

---
+++ MVapi.java
@@ -0,0 +1,17 @@
+package eu.majnvers.mvapi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MVapi extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
